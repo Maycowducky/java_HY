9f1b7644796498ad89bd122e112ff45e45090d63
Maycowducky <maycowduck@kakao.com>
2023-05-31 12:40:25 +0900

    자바 문자열

---
+++ java_hy/src/day2/variable/StringEx.java
@@ -0,0 +1,19 @@
+package day2.variable;
+
+public class StringEx {
+
+	public static void main(String[] args) {
+		/* String 클래스는 문자열을 수정하는게 아니라 새로 생성 */
+		String str1 = null;
+		String str2 = "";
+		String str3 = "A"; //char ch = 'A';와 다름
+		String str4 = "안녕하세요."; //여러글자 가능
+		System.out.println(str1); //null이라는 문자열이 아님.
+		System.out.println(str2); // (str2. 하면 나열되는것들 나중에 사용가능)
+		System.out.println(str3);
+		System.out.println(str4);
+		
+	
+	}
+
+}
