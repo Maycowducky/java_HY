f045fa2490040a0bf47a3771ab8ecf00c084c51f
Maycowducky <maycowduck@kakao.com>
2023-05-31 15:14:41 +0900

    숙제

--- java_hy/src/day2/homework/Ex1.java
+++ java_hy/src/day2/homework/Ex1.java
@@ -3,8 +3,14 @@ package day2.homework;
 public class Ex1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//콘솔에서 정수 2개와 문자 (산술연산자)를 입력 받아 출력하는 코드를 작성하세요.
+		/*
+		 * 예:
+		 * 두정수와 산술연산자를 입력하세요 (예:1 + 2) :
+		 * [입력] 1 + 2 [엔터]
+		 * [출력] 1 + 2
+		 */
+		
 	}
 
 }
