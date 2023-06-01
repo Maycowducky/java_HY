3a04710d06e0409e02e37c0d44f3d9ce8e2b5008
Maycowducky <maycowduck@kakao.com>
2023-06-01 14:59:08 +0900

    day 3 homework

---
+++ java_hy/src/day3/homework/Ex1_Conditional.java
@@ -0,0 +1,16 @@
+package day3.homework;
+
+public class Ex1_Conditional {
+
+	public static void main(String[] args) {
+		/*성별(M:남성,W:여성)를 입력받아 M이면 남성을, W이면 여성을 출력하는 코드를 작성하세요.
+		 *input gender :
+		 *M
+		 *Are you a woman? false
+		 *W
+		 *Are you a woman? true 
+		 */
+
+	}
+
+}
