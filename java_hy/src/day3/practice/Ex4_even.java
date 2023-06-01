f5ad73c9861d2c67535a4f313376724193bb14af
Maycowducky <maycowduck@kakao.com>
2023-06-01 14:58:55 +0900

    day 3 if, else 예제

--- java_hy/src/day3/practice/Ex4_even.java
+++ java_hy/src/day3/practice/Ex4_even.java
@@ -9,13 +9,26 @@ public class Ex4_even {
 		 * num을 2로 나누었을 때 나머지가 0이 아니면 odd number라고 출력
 		 * num을 2로 나누었을 때 나머지가 0이면 even number라고 출력
 		 */
+		int num;
 		Scanner sc = new Scanner(System.in);
 			System.out.println("input number : ");
-		int num = sc.nextInt();
+		num = sc.nextInt();
+		
+		/* num를 2로 [나누었을 때 나머지가] 0이 아니다)
+		 * num % 2 != 0)
+		 */
+		
+		/*
+		 * if num % 2 !=0) {
+		 * System.out.println(num + " is a even number.");
+		 * if num % 2 ==0) {
+		 * System.out.println(num + " is a odd number.");
+		 */
 		
 		if(num %2 == 0) {
 			System.out.println(num + " is a even number.");
 		}
+		//
 		else{
 			System.out.println(num + " is a odd number.");
 		}
