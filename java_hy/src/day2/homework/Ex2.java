d7d91fd3625febf8d188bb534e111df1602f43ad
Maycowducky <maycowduck@kakao.com>
2023-06-01 09:40:56 +0900

    day2 숙제

--- java_hy/src/day2/homework/Ex2.java
+++ java_hy/src/day2/homework/Ex2.java
@@ -1,5 +1,7 @@
 package day2.homework;
 
+import java.util.Scanner;
+
 public class Ex2 {
 
 	public static void main(String[] args) {
@@ -14,18 +16,29 @@ public class Ex2 {
 		 * 1 / 2 = 0.5
 		 * 1 % 2 = 1
 		 */
+		//안내 문구 출력
+		System.out.println("Please enter two integers.");
+		//콘솔에서 입력받을 수 있는 스캐너 생성
+		Scanner sc = new Scanner(System.in);
+		//두 정수를 입력 받음
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+		//두 정수의 산술 연산을 계산
+		int sum = num1 + num2;
+		int sub = num1 - num2;
+		int mul = num1 * num2;
+		double div = num1 / (double)num2;
+		int mod = num1 % num2;
+		//결과를 콘솔에 출력
+		System.out.println(num1 + "+" + num2 + "=" + sum);
+		System.out.println(num1 + "-" + num2 + "=" + sub);
+		System.out.println(num1 + "*" + num2 + "=" + mul);
+		System.out.println(num1 + "/" + num2 + "=" + div);
+		System.out.println(num1 + "%" + num2 + "=" + mod);
 		
-		int num3 = 2, num4 = 4;
-		int sum = num3 + num4;
-		int sub = num3 - num4;
-		int mul = num3 * num4;
-		double div = (double)num3 / num4;
-		int mod = num3 % num4;
-		System.out.println("" + num3 + '+' + num4 + "=" + sum);
-		System.out.println("" + num3 + '-' + num4 + "=" + sub);
-		System.out.println("" + num3 + '*' + num4 + "=" + mul);
-		System.out.println("" + num3 + '/' + num4 + "=" + div);
-		System.out.println("" + num3 + '%' + num4 + "=" + mod);
+		sc.close();
+				
+		
 	}
 
 }
