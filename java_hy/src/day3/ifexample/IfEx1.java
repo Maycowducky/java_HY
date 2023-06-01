da5cb6c73fa3c69e68a70bbaa20877c2e554c88c
Maycowducky <maycowduck@kakao.com>
2023-06-01 14:20:35 +0900

    If (조건식) 예제

---
+++ java_hy/src/day3/ifexample/IfEx1.java
@@ -0,0 +1,28 @@
+package day3.ifexample;
+
+public class IfEx1 {
+
+	public static void main(String[] args) {
+		//정수 num가 0이면 0입니다라고 출력하는 코드를 작성
+		/*if(조건식) {
+			실행문;*/
+		//정수 num가 0이다
+		//if (정수 num가 0이다) {
+		//정수 num가 0과 같다
+		//if (정수 num가 0과 같다) {
+			//0입니다라고 출력;
+		
+		int num = 0;
+		if(num == 0) {
+			//0입니다 출력;
+		System.out.println("0입니다.");
+		}
+		//정수 num가 0이 아니면 num is not 0라고 출력하는 코드를 작성하세요.
+		//num가 0이 아니다
+		//num가 0과 같지 않다
+		if(num != 0) {
+			System.out.println(num + " is not 0");
+		}
+	}
+	
+}
