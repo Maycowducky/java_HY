0907f2e89bef32348c2e0ada42ba7364cdd67367
Maycowducky <maycowduck@kakao.com>
2023-06-01 14:45:45 +0900

    day3 if, else 조건문 예제

---
+++ java_hy/src/day3/practice/Ex4_even.java
@@ -0,0 +1,25 @@
+package day3.practice;
+
+import java.util.Scanner;
+
+public class Ex4_even {
+
+	public static void main(String[] args) {
+		/*정수 num을 입력받아 num이 홀수인지 짝수인지 판별하는 코드를 작성하세요.
+		 * num을 2로 나누었을 때 나머지가 0이 아니면 odd number라고 출력
+		 * num을 2로 나누었을 때 나머지가 0이면 even number라고 출력
+		 */
+		Scanner sc = new Scanner(System.in);
+			System.out.println("input number : ");
+		int num = sc.nextInt();
+		
+		if(num %2 == 0) {
+			System.out.println(num + " is a even number.");
+		}
+		else{
+			System.out.println(num + " is a odd number.");
+		}
+			
+		sc.close();
+	}
+}
