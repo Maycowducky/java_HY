1df3fa3670c71bdabb5507ae67aa0c83d2df0806
Maycowducky <maycowduck@kakao.com>
2023-06-01 12:17:38 +0900

    조건 연산자 예제

--- java_hy/src/day3/operator/ConditionalEx.java
+++ java_hy/src/day3/operator/ConditionalEx.java
@@ -3,11 +3,11 @@ package day3.operator;
 public class ConditionalEx {
 
 	public static void main(String[] args) {
-		//나이가 20세 이상이면 Adult, 아니면 minor를 출력하는 코드를 작성하세요.
+		//나이가 20세 이상이면 Adult, 아니면 Minor를 출력하는 코드를 작성하세요.
 		int age = 10;
 		String str;
 		
-		str = age >= 20 ? "Adult" : "minor";
+		str = age >= 20 ? "adult" : "minor";
 		System.out.println(age + " years old is a " + str);
 	}
 
