32696e44ddc62850a79ba988f18f344fc903f231
Maycowducky <maycowduck@kakao.com>
2023-06-01 09:26:25 +0900

    day2 숙제

--- java_hy/src/day2/homework/Ex1.java
+++ java_hy/src/day2/homework/Ex1.java
@@ -1,5 +1,7 @@
 package day2.homework;
 
+import java.util.Scanner;
+
 public class Ex1 {
 
 	public static void main(String[] args) {
@@ -11,13 +13,17 @@ public class Ex1 {
 		 * [출력] 1 + 2
 		 */
 		
-		String str="";
-		char op = '+';
-		int num1 = 2, num2 = 5;
-		str = "" + num1 + op + num2 + "=" + (num1+num2);
-		System.out.println(str);
+		int num1, num2;
+		char operator;
+		Scanner sc = new Scanner(System.in);
 		
+		System.out.println("두 정수와 산술연산자를 입력하세요 (예:1 + 2)");
+		num1 = sc.nextInt();
+		operator = sc.next().charAt(0);
+		num2 = sc.nextInt();
+		System.out.println("" + num1 + operator + "" + num2);
 		
+		sc.close();
 	}
 
 }
