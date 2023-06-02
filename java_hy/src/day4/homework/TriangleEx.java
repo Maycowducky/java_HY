2251171c939f1024c7593905cedf0b70e0ba5611
Maycowducky <maycowduck@kakao.com>
2023-06-02 14:45:19 +0900

    주말 과제

---
+++ java_hy/src/day4/homework/TriangleEx.java
@@ -0,0 +1,17 @@
+package day4.homework;
+
+public class TriangleEx {
+
+	public static void main(String[] args) {
+		/*세 변의 길이를 입력받아 세 변으로 삼각형을 만들 수 있는지 확인하는 코드를 작성
+		 * 세 변 중 작은 두 변의 길이의 합이 큰 변보다 커야 삼각형을 만들 수 있다.
+		 *input 3 interger num :
+		 * 1 2 3
+		 * unable to create triangle.
+		 * input 3 integer num 
+		 * 2 2 3
+		 * Able to create triangle.
+		 */
+	}
+
+}
