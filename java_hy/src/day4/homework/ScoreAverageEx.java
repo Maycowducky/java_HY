9c27bf7a7e2e83397c362156f898e4c968a9cc7a
Maycowducky <maycowduck@kakao.com>
2023-06-02 15:17:32 +0900

    주말 과제

--- java_hy/src/day4/homework/ScoreAverageEx.java
+++ java_hy/src/day4/homework/ScoreAverageEx.java
@@ -1,11 +1,23 @@
 package day4.homework;
 
+import java.util.Scanner;
+
 public class ScoreAverageEx {
 
 	public static void main(String[] args) {
 		/*국어, 영어, 수학 성적을 입력받아 평균을 출력하는 코드를 작성하세요.
 		 *성적은 정수.
 		 */
+		int Lit,Eng,Math;
+		Scanner sc= new Scanner(System.in);
+		System.out.println("input score: ");
+		Lit = sc.nextInt();
+		Eng = sc.nextInt();
+		Math = sc.nextInt();
+		int num = (Lit+Eng+Math)/3;
+		System.out.println(num);
+		
+		sc.close();
 	}
 
 }
