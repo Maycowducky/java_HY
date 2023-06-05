aaf23480f7cd880191947566f5437ec7cdbd4c8f
Maycowducky <maycowduck@kakao.com>
2023-06-05 10:57:04 +0900

    스위치문 PPT class 노트

---
+++ java_hy/src/day5/practice/SwitchEx.java
@@ -0,0 +1,26 @@
+package day5.practice;
+
+public class SwitchEx {
+
+	public static void main(String[] args) {
+		/*switch(변수 또는 식) {
+		case 값1 :
+			실행문 1;
+			break;
+		case 값2 :
+			실행문2;
+			break;
+		default:
+			실행문3;
+		}
+		//값에 들어갈수 있는 자료형은 정수, 문자, 문자열만 가능. 실수 불가능
+		 * case 옆에 들어가는 값은 리터럴 또는 상수만 가능.
+		 * switch문에서 break가 없으면 밑에 있는 case에 있는 실행문이 실행.
+		 * break를 이용하면 실행문이 같은 값들을 묶어서 처리.
+		 * case 값1: case 값2: case 값3:
+		 * 	실행문1;
+		 * 조건이 변수가 특정 값일때 처리하는 경우 사용
+			*/
+	}
+
+}
