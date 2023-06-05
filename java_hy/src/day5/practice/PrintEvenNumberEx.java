ad90c00a78bb37438755fd0281b7d31f3a8c3a63
Maycowducky <maycowduck@kakao.com>
2023-06-05 12:21:40 +0900

    day 5 while문 짝수 예제

---
+++ java_hy/src/day5/practice/PrintEvenNumberEx.java
@@ -0,0 +1,52 @@
+package day5.practice;
+
+import java.util.Scanner;
+
+public class PrintEvenNumberEx {
+
+	public static void main(String[] args) {
+		/*10 이하의 짝수를 출력하는 코드를 작성
+		 * 2 , 4 , 6 , 8 , 10
+		 */
+		/*방법 1.
+		 * 반복횟수: i는 1부터 5까지 1씩증가
+		 * 규칙성:2*i를 출력
+		 */
+		int i = 1;
+		while(i <= 5) {
+			System.out.println(2 * i);
+			++i;
+		/*int i; //scanner 예제
+		Scanner sc = new Scanner(System.in);
+			System.out.println("input number: ");
+		i=sc.nextInt();
+			sc.close();
+		while (i <= 5) {
+			System.out.println(2 * i);
+			i++;*/
+		}
+		System.out.println("============");
+		/*방법 2.
+		 * 반복횟수: i는 2부터 10까지 2씩증가
+		 * 규칙성:i를 출력
+		 */
+		i = 2;
+		while(i <= 10) {
+			System.out.println(i);
+			i = i + 2; // i += 2;
+		}
+		System.out.println("============");
+		/*방법 3.
+		 * 반복횟수: i는 1부터 10까지 1씩증가
+		 * 규칙성:i가 짝수이면 i를 출력
+		 */
+		i = 1;
+		while(i <= 10) {
+			if(i % 2 ==0) {
+				System.out.println(i);
+			}
+			++i;
+		}
+	}
+
+}
