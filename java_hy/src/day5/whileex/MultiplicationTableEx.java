80f5a7b1970b7107ec22ce0dbf6c973b4d32edbf
Maycowducky <maycowduck@kakao.com>
2023-06-05 12:22:36 +0900

    day 5 while문으로 2단 만들기

---
+++ java_hy/src/day5/whileex/MultiplicationTableEx.java
@@ -0,0 +1,26 @@
+package day5.whileex;
+
+import java.util.Scanner;
+
+public class MultiplicationTableEx {
+
+	public static void main(String[] args) {
+		/*	2 x 1 = 2
+		 * 2 x 2 = 4
+		 * ...
+		 * 2 x 9 = 18
+		 * 반복횟수 : i는 1부터 9까지 1씩 증가
+		 * 규칙성 : 2 x i = 2*i를 출력
+		 */
+		int i;
+		Scanner sc = new Scanner(System.in);
+			System.out.println("input number: ");
+		i=sc.nextInt();
+			sc.close();
+		while (i <= 9) {
+			System.out.println("2 X " + i + " = " + 2 * i);
+			i++;
+		}
+	}
+
+}
