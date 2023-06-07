31290ee58f0303af2edc956188c271fd62ea2ce3
Maycowducky <maycowduck@kakao.com>
2023-06-07 09:47:44 +0900

    day 5 random문 과제

--- java_hy/src/day5/homework/UpdownEx.java
+++ java_hy/src/day5/homework/UpdownEx.java
@@ -14,26 +14,29 @@ public class UpdownEx {
 		 * input : 45
 		 * Good!
 		 */
-		int i;
 		int min = 1, max = 100;
-		int random = (int)(Math.random() * (max - min + 1) + min);
+		int num = min - 1;//1~100이 아닌 정수로 초기화 하면 됨. 최소값 -1을 해서 랜덤으로 나올 수 없는 수
+		int random;
 		Scanner sc = new Scanner(System.in);
-		System.out.println("input: ");
+
+		//랜덤한 수를 생성 (및 출력)
+		random = (int)(Math.random()*(max - min + 1) + min);
 		System.out.println(random);
-		i= sc.nextInt();
-		while(i != 3) {
-			
-			
-			switch(i) {
-			case 46-100:
+
+		//반복문 : 맞출때까지 => 입력한 정수가 정답이 아니면 반복
+		while(random != num) {
+			//정수를 입력
+			System.out.println("input(1~100) : ");
+			num = sc.nextInt();
+
+			//정수와 정답을 비교하여 판별
+			//num가 random보다 크면 Down!,아니고 num가 random보다 작으면 Up!, 아니면 Good!
+			if(num > random) {
 				System.out.println("Down!");
-				break;
-			case 1-44:
+			}else if(num < random) {
 				System.out.println("Up!");
-				break;
-			case 45:
-				System.out.println("Good!" + " Program Exit!");
-				break;
+			}else {
+				System.out.println("Good!");
 			}
 		}
 		sc.close();
