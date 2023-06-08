0d8f7ec664504ad4b29736f225915fd492e27209
Maycowducky <maycowduck@kakao.com>
2023-06-08 14:56:55 +0900

    day 7 산술연산자 method 예제

--- java_hy/src/day7/method/ArithmeticMethodEx.java
+++ java_hy/src/day7/method/ArithmeticMethodEx.java
@@ -3,13 +3,35 @@ package day7.method;
 public class ArithmeticMethodEx {
 
 	public static void main(String[] args) {
+		
 		System.out.println(sum(1,2));
 		System.out.println(sub(1,2));
 		System.out.println(mul(1,2));
 		System.out.println(div(1,2));
 		System.out.println(mod(1,2));
-		
+		sum2(1,2);
+		//System.out.println(1+2);
+		//1+2+3 //좋은 method 형태는 재사용이 가능할때
+		int num1=1, num2=2,num3=3;
+		int res = sum(num1,num2);
+		res = sum(res,num3);
+		System.out.println(res);
 	}
+	/**
+	 * 주어진 num1, num2의 합을 출력하는 메소드
+	 * (더하는 기능과 + 출력하는 기능) 같이 들어있기때문에 좋은 method 형태가 아님
+	 * @param num1 정수1
+	 * @param num2 정수2
+	 */
+	public static void sum2(int num1, int num2) {
+		System.out.println(num1 + num2);
+	}
+	/**
+	 * 주어진 num1, num2의 합을 알려주는 메소드
+	 * @param num1 정수1
+	 * @param num2 정수2
+	 * @return num1과 num2의 합
+	 */
 	
 	public static int sum(int num1, int num2) {
 		return num1 + num2;
