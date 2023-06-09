bc0f5bbe01ac4b838b6d77be41ce62f0720e5334
Maycowducky <maycowduck@kakao.com>
2023-06-09 10:33:37 +0900

    day 8 데이터 오버로딩 - method

---
+++ java_hy/src/day8/dataoverloading/Ex1.java
@@ -0,0 +1,39 @@
+package day8.dataoverloading;
+
+public class Ex1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println(1);
+		System.out.println("1");
+		System.out.println(1.1);
+		System.out.println('c');
+		System.out.println(sum(1,1));
+		System.out.println(sum(1.1,1));
+		System.out.println(sum(1,1.1));
+		System.out.println(sum(1.1,1.1));
+		System.out.println(sum(1,2,3));
+	}
+	public static int sum(int num1, int num2) {
+		return num1 + num2;
+	}
+	//매개변수의 타입이 다른경우
+	public static double sum(double num1, int num2) {
+		return num1 + num2;
+	}
+	public static double sum(int num1, double num2) {
+		return num1 + num2;
+	}
+	public static double sum(double num1, double num2) {
+		return num1 + num2;
+	}
+	//매개변수의 개수가 다른경우
+	public static int sum(int num1, int num2,int num3) {
+		return num1 + num2 + num3;
+	}
+	/*
+	//return 타입만 다른 method는 overloading이 될 수 없다.
+	public static double sum(int num1, int num2) {
+		return num1 + num2;
+	}*/
+}
