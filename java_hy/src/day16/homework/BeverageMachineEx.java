dc48ce77132d2fe7fbff258c49ed7354ae1c4ac5
Maycowducky <maycowduck@kakao.com>
2023-06-21 14:41:51 +0900

    day 16 vending machine 과제

--- java_hy/src/day16/homework/BeverageMachineEx.java
+++ java_hy/src/day16/homework/BeverageMachineEx.java
@@ -6,11 +6,21 @@ public class BeverageMachineEx {
 		/* 음료수를 뽑는 자판기 프로그램을 작성하세요.
 		 * 단, 음료수 자판기에 음료수는 사이다, 콜라, 환타가 고정이라고 가정.
 		 * 메뉴를 선택하면 음료수와 잔액을 출력.
-		 * 
+		 * 메뉴
 		 * 1. 금액 투입
 		 * 2. 메뉴 선택
 		 * 3. 제품 입고
 		 * 4. 프로그램 종료
+		 * 메뉴 선택 : 2
+		 * 1. 사이다
+		 * 2. 콜라
+		 * 3. 환타
+		 * 음료 선택 : 1
+		 * 사이다를 선택 했습니다.
+		 * 사이다가 나옵니다. [금액이 충분한 경우]
+		 * 잔돈 얼마가 나옵니다.
+		 * 금액이 부족합니다. [금액이 부족한 경우]
+		 * 잔돈 얼마가 나옵니다.
 		 */
 		private static int cash;
 		private static int cokeStock;
@@ -18,10 +28,10 @@ public class BeverageMachineEx {
 		private static int fantaStock;
 		
 		public static void main(String[] args) {
-			cash = 0;
-			cokeStock = 5;
-			spriteStock = 5;
-			fantaStock = 5;
+			cash = 30000;
+			cokeStock = 15;
+			spriteStock = 15;
+			fantaStock = 15;
 			
 			Scanner sc = new Scanner(System.in);
 			int choice;
@@ -54,12 +64,12 @@ public class BeverageMachineEx {
 		
 		}
 		private static void printMenu() {
-			System.out.println("======Vending Machine=====");
+			System.out.println("======메  뉴=====");
 			System.out.println("1. 금액 투입");
 			System.out.println("2. 메뉴 선택");
 			System.out.println("3. 제품 입고");
 			System.out.println("4. 프로그램 종료");
-			System.out.println("==========================");
+			System.out.println("================");
 			
 		}
 		private static void insertCash(Scanner sc) {
