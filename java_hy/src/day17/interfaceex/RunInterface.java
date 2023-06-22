ecb1f47d3cc4d2758d85247a05579beb869dba01
Maycowducky <maycowduck@kakao.com>
2023-06-22 12:43:53 +0900

    day 17 interface: shopManager 예제

---
+++ java_hy/src/day17/interfaceex/RunInterface.java
@@ -0,0 +1,10 @@
+package day17.interfaceex;
+
+public interface RunInterface{
+	/*public final static*/ int NUM = 10; // 클래스 상수 → 클래스를 이용하여 호출 
+	/*public abstract*/ void run();
+	default void stop() {
+		System.out.println("Program Deactivated");
+	}
+	
+}
