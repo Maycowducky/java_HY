2a4971bc9c73c6322b20de21e498a07852d47066
Maycowducky <maycowduck@kakao.com>
2023-06-23 14:43:28 +0900

    day 18 StringTokenizer 예제

---
+++ java_hy/src/day18/string/StringTokenizerEx.java
@@ -0,0 +1,21 @@
+package day18.string;
+
+import java.util.StringTokenizer;
+
+public class StringTokenizerEx {
+
+	public static void main(String[] args) {
+		
+		
+		String fruits ="apple,orange,banana";
+		
+		StringTokenizer st = new StringTokenizer(fruits, ",");
+		
+		//hasMoreTokens: 다음 토큰이 있는지 없는 알려줌. 없다는건 끝났다는 의미
+		while(st.hasMoreTokens()) {
+								//nextToken : 토큰을 가져옴
+			System.out.println(st.nextToken());
+		}
+	}
+
+}
