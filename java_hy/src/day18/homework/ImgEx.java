4788b590587432b88af064c0a373ddcd1554bdca
Maycowducky <maycowduck@kakao.com>
2023-06-23 14:42:27 +0900

    day 18 주말과제

---
+++ java_hy/src/day18/homework/ImgEx.java
@@ -0,0 +1,14 @@
+package day18.homework;
+
+public class ImgEx {
+	public static void main(String[] args) {
+		/* 파일명을 입력받아 파일이 이미지인지 확인하는 코드를 작성하세요.
+		 * 이미지 확장자 : jpg, bmp, png (더있지만 간단하게 3개만 체크)
+		 * input : dog.jpg
+		 * dog.jpg : Image
+		 * 
+		 * input : dog.exe
+		 * dog.exe : Not image
+		 */
+	}
+}
