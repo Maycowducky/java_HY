4df7a72ba6b38a045136b33d7ef6dd7d5d2a98ca
Maycowducky <maycowduck@kakao.com>
2023-06-27 14:34:52 +0900

    day 20 과제 List로 수정하기

---
+++ java_hy/src/day20/homework/vocabulary/run/Run.java
@@ -0,0 +1,13 @@
+package day20.homework.vocabulary.run;
+
+import day13.homework.controller.VocabularyNoteController;
+
+public class Run {
+
+	public static void main(String[] args) {
+		VocabularyNoteController controller = new VocabularyNoteController();
+		controller.run();
+		
+	}
+
+}
