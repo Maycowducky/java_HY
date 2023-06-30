c0ccc944f467b4862cbdcd748ae0b0fe0094df03
Maycowducky <maycowduck@kakao.com>
2023-06-30 15:03:12 +0900

    day 23 InputOutput 예제

---
+++ java_hy/src/day23/practice/InputStreamEx.java
@@ -0,0 +1,43 @@
+package day23.practice;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+public class InputStreamEx {
+
+	public static void main(String[] args) {
+		//정수를 1부터 10까지 파일에 저장하는 코드를 작성하세요.
+		// 파일명은 output1.txt
+		
+		try(FileInputStream fis = new FileInputStream("output1.txt")){
+			int data;
+			do {
+				data = fis.read();
+				if(data == -1) {
+					break;
+				}
+				System.out.print(data);
+			}while(true);
+			System.out.println();
+			System.out.println("File reader success");
+		} catch (FileNotFoundException e) {
+			System.out.println("File Not Found!!");
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		try(
+				FileInputStream fis = new FileInputStream("output2.txt");
+				ObjectInputStream ois = new ObjectInputStream(fis)){
+			System.out.println(ois.readLine());
+				for(int i = 1; i <=10; i++) {
+					System.out.print(ois.readInt());
+				}	
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+	}
+
+}
---
+++ java_hy/src/day23/practice/OutputStreamEx.java
@@ -0,0 +1,37 @@
+package day23.practice;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+public class OutputStreamEx {
+
+	public static void main(String[] args) {
+		//정수를 1부터 10까지 파일에 저장하는 코드를 작성하세요.
+		// 파일명은 output1.txt
+		
+		try(FileOutputStream fos = new FileOutputStream("output1.txt")){
+			for(int i = 1; i <=10; i++) {
+				fos.write(i);
+			}
+			System.out.println("File writer success");
+		} catch (FileNotFoundException e) {
+			System.out.println("File Not Found!!");
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		try(
+			FileOutputStream fos = new FileOutputStream("output2.txt");
+			ObjectOutputStream oos = new ObjectOutputStream(fos)){
+			System.out.println("");
+			for(int i = 1; i <=10; i++) {
+				oos.writeInt(i);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
