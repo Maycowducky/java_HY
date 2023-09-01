8993d211638bfa219d0438fcb401679bdf26e1e4
Maycowducky <maycowduck@kakao.com>
2023-09-01 10:55:22 +0900

    SPRING : 파일 업로드 구현

--- Spring Projects/spring/src/main/java/kr/kh/spring/service/BoardServiceImp.java
+++ Spring Projects/spring/src/main/java/kr/kh/spring/service/BoardServiceImp.java
@@ -1,32 +1,62 @@
 package kr.kh.spring.service;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import kr.kh.spring.dao.BoardDAO;
 import kr.kh.spring.pagination.Criteria;
+import kr.kh.spring.util.UploadFileUtils;
 import kr.kh.spring.vo.BoardVO;
+import kr.kh.spring.vo.FileVO;
 import kr.kh.spring.vo.MemberVO;
 
 @Service
-public class BoardServiceImp implements BoardService {
+public class BoardServiceImp implements BoardService{
 
 	@Autowired
 	BoardDAO boardDao;
+	
+	String uploadPath = "D:\\uploadfiles";
 
 	@Override
-	public boolean insertBoard(BoardVO board, MemberVO user) {
+	public boolean insertBoard(BoardVO board, MemberVO user, MultipartFile[] files) {
 		if(user == null || user.getMe_id() == null) {
-			
 			return false;
 		}
-		if(board == null || board.getBo_title() == null || board.getBo_title().length() == 0) {
+		if(board == null || board.getBo_title()==null || board.getBo_title().length() == 0) {
 			return false;
 		}
 		board.setBo_me_id(user.getMe_id());
-		return boardDao.insertBoard(board);
+		if(!boardDao.insertBoard(board)) {
+			return false;
+		}
+		//첨부파일을 업로드
+		if(files == null || files.length == 0) {
+			return true;
+		}
+		for(MultipartFile file : files) {
+			//첨부파일을 서버에 업로드 하고, DB에 저장
+			uploadFileAndInsert(file, board.getBo_num());
+		}
+		return true;
+	}
+
+	private void uploadFileAndInsert(MultipartFile file, int bo_num) {
+		if(file == null || file.getOriginalFilename().length() == 0) {
+			return;
+		}
+		try {
+			String fi_name = UploadFileUtils.uploadFile(uploadPath, file.getOriginalFilename(), file.getBytes());
+			FileVO fileVo = new FileVO(bo_num, fi_name, file.getOriginalFilename());
+			boardDao.insertFile(fileVo);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
 	}
 
 	@Override
@@ -59,7 +89,5 @@ public class BoardServiceImp implements BoardService {
 			return;
 		}
 		boardDao.updateBoardViews(bo_num);
-		
 	}
-
 }
