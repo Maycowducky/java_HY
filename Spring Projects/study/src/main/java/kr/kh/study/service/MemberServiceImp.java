b544acb8fa8153a41f8c29e41e34cd8fb2922a46
Maycowducky <maycowduck@kakao.com>
2023-09-11 12:39:38 +0900

    Spring 복습 : 자동 로그인 구현

--- Spring Projects/study/src/main/java/kr/kh/study/service/MemberServiceImp.java
+++ Spring Projects/study/src/main/java/kr/kh/study/service/MemberServiceImp.java
@@ -62,6 +62,20 @@ public class MemberServiceImp implements MemberService{
 		return null;
 	}
 
+	@Override
+	public void updateMemberSession(MemberVO user) {
+		if(user == null) {
+			return;
+		}
+		memberDao.updateMemberSession(user);
+	}
+
+	@Override
+	public MemberVO getMemberBySession(String me_session_id) {
+		
+		return memberDao.selectMemberBySession(me_session_id);
+	}
+
 	
 }
 
